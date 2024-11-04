3f06f4e7412aaabec85db68e607aae4e39c6d44d
Father <JOBY@DESKTOP-4V6ESC0>
2024-11-04 12:22:11 +0100

    + klar med this and that

--- Program.cs
+++ Program.cs
@@ -26,7 +26,31 @@
 
         public static void SearchBooking()
         {
+            bool yearHasBookings = false;
+            Console.WriteLine("What year of bookings do you wanna see?");
+            int choiseYear = int.Parse(Console.ReadLine());
+            foreach (var local in locals)
+            {
+                foreach (var bookings in local.Bookings)
+                {
 
+                    if (bookings.StartTime.Year == choiseYear)
+                    {
+                        yearHasBookings = true;
+                        Console.WriteLine($"Room: {bookings.Name}, Boked by {bookings.BookingName} ");
+                        Console.WriteLine($"{bookings.StartTime:Y} {bookings.StartTime:t} - {bookings.StartTime.Add(bookings.Duration):t}");
+                        Console.WriteLine();
+
+                    }
+
+
+                }
+            }
+            if (!yearHasBookings)
+            {
+                Console.WriteLine($"No bookings were found for the year {choiseYear}.");
+            }
+            GobackPause();
         }
     }
 }
