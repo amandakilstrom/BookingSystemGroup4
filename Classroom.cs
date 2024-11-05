0163d01e12604217a50e4c28feb532a4346d75d0
Joby-0 <johanjohan.bylander@gmail.com>
2024-11-05 13:48:22 +0100

    Revert "skapa nya salar"

--- Classroom.cs
+++ Classroom.cs
@@ -1,26 +1,12 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BookingSystemGroup4
 {
     internal class Classroom : Local
     {
-        public Classroom()
-        {
-            Bookings = new List<Local>();
-        }
-
-        
-        public override void BookRoom(string name, DateTime startTime, TimeSpan duration, string bookingName)
-        {
-
-            base.BookRoom(name, startTime, duration, bookingName); //kör base class metod
-
-        }
     }
 }
-
